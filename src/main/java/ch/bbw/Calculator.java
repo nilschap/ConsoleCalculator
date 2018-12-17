5770b4519c8e4ed5be383cf2f26225da02e765df
Chapman Nils, GHR-OSA-NEX-15 <Nils.Chapman@swisscom.com>
2018-12-17 08:04:30 +0100

    Prepare test for subtraktion

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int summe(int summand1,int summand2){
         return summand1+summand2;
     }
+
+    public int subtraktion(int value1, int value2){
+        return 0;
+    }
 }
